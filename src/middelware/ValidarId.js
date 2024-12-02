b284c09b444aeefb7ef4b09fc1278eb4f6073739
Cristian Luna <cristiansebastian.luna@estudiantes.unahur.edu.ar>
2024-12-01 21:15:29 -0300

    validador sin cod comentado

--- src/middelware/ValidarId.js
+++ src/middelware/ValidarId.js
@@ -3,17 +3,11 @@ const mongoose = require('mongoose')
 const validarId = (Modelo) => {
     return async (req, res, next) => {
         const id = req.params.id;
-        // Verifica si el ID es válido
         if (!mongoose.Types.ObjectId.isValid(id)) {
            return res.status(400).json({ error: "ID no válido" });
         }
-        const modelo= Modelo.findById(id)
-        if (!modelo) {
-            return res.status(404).json({ error: `${modelo.name} no encontrado` });
-        }
-
         req.modelo = Modelo;
-       
+
         next();
     };
 };
